522097fe9120df5acf6482f185c70ab63ef10094
RMI78 <romain.escrime@gmail.com>
2022-10-04 14:28:06 +0200

    :rocket: 2nd active button available

--- index.js
+++ index.js
@@ -3,12 +3,25 @@
 //   button++;
 // };
 
+
 window.addEventListener('load', () => {
   const counter = document.querySelector('#value');
   const triggerButton = document.querySelector('#clickme')
+  const activeBonusButton = document.querySelector('#active_bonus')
+  let bonus=0;
 
+
+  
   triggerButton.addEventListener('click', () => {
-    counter.textContent++;
+    //put variable in html file then get it at each function call with the following line
+    let number = parseInt(counter.textContent);
+    number++;
+    counter.textContent = Number(number+bonus);
   })
+  activeBonusButton.addEventListener('click', ()=>{
+    bonus++;
+    // console.log(bonus)
+  })
+
 
 })
