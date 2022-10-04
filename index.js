6e1c148d395c3087bf0909f4b5b83707a62ba37c
RMI78 <romain.escrime@gmail.com>
2022-10-04 14:01:14 +0200

    functionnal base game

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var button = document.getElementById("clickme"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-  button.innerHTML = "Click me: " + count;
-};
+// function increment() {
+//   var button = document.getElementById("value").value;
+//   button++;
+// };
+
+window.addEventListener('load', () => {
+  const counter = document.querySelector('#value');
+  const triggerButton = document.querySelector('#clickme')
+
+  triggerButton.addEventListener('click', () => {
+    counter.textContent++;
+  })
+
+})
