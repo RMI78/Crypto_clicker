17a8650cba1e2eeb114bda12250e49d891c61c6d
RMI78 <romain.escrime@gmail.com>
2022-10-04 14:57:42 +0200

    implemented active bonus

--- index.js
+++ index.js
@@ -8,18 +8,35 @@ window.addEventListener('load', () => {
   const counter = document.querySelector('#value');
   const triggerButton = document.querySelector('#clickme')
   const activeBonusButton = document.querySelector('#active_bonus')
-  let bonus=0;
-
-
+  const activeBonusCost = document.querySelector('#active_bonus_cost')
+  const currentActiveBonus = document.querySelector('#current_active_bonus')
+  const messageArea = document.querySelector('#message_area')
+  
+  
   
   triggerButton.addEventListener('click', () => {
     //put variable in html file then get it at each function call with the following line
+    let bonus=parseInt(currentActiveBonus.textContent);
     let number = parseInt(counter.textContent);
     number++;
     counter.textContent = Number(number+bonus);
+    messageArea.textContent = "Mining Money ^^"
   })
   activeBonusButton.addEventListener('click', ()=>{
-    bonus++;
+    let number = parseInt(counter.textContent)
+    let bonus=parseInt(currentActiveBonus.textContent);
+    let costActiveBonus=Number(activeBonusCost.textContent);
+    if(number>=costActiveBonus){
+      bonus++;
+      number-=costActiveBonus;
+      costActiveBonus+=10
+      activeBonusCost.textContent = costActiveBonus; 
+      counter.textContent = number
+      currentActiveBonus.textContent = bonus
+    }
+    else{
+      messageArea.textContent = "not enough money :'("
+    }
     // console.log(bonus)
   })
 
