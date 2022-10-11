864b2afc9e4194016ef95a8a52b2b76de6080192
RymABED <rysha.sirine@gmail.com>
2022-10-11 17:01:49 +0200

    add infinite scroll

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var button = document.getElementById("clickme"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-  button.innerHTML = "Click me: " + count;
-};
+window.addEventListener('load', () => {
+  const counter = document.querySelector('#value');
+  const triggerButton = document.querySelector('#clickme')
+  const activeBonusButton = document.querySelector('#active_bonus')
+  let bonus=0;
+
+
+  
+  triggerButton.addEventListener('click', () => {
+    //put variable in html file then get it at each function call with the following line
+    let number = parseInt(counter.textContent);
+    number++;
+    counter.textContent = Number(number+bonus);
+    const element = document.getElementById('abcd');
+    if( (Number(number+bonus) >= 2) && (Number(number+bonus)<20)) 
+      element.innerHTML = "gooooood!!!!!";
+    else if( (Number(number+bonus)>=20) && (Number(number+bonus)<50)) 
+      element.innerHTML = "u can now buy an active bonus!!!";
+    else 
+      element.innerHTML = " "
+  })
+
+  activeBonusButton.addEventListener('click', ()=>{
+    bonus++;
+    // console.log(bonus)
+  })
+})
